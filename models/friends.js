cae74e8604709113239376b54d08055cb690f6ee
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-12-11 15:36:25 -0500

    schema name to unigue: true

--- models/friends.js
+++ models/friends.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostCarefulFriend = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   entry: String,
   img: String,
-  userId: { type: Schema.ObjectId, ref: "User", required: true },
+  // userId: { type: Schema.ObjectId, ref: "User", required: true, unique: true },
 });
 
 const Friend = mongoose.model("Friend", PostCarefulFriend);
