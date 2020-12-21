193a4de4b765fc1ba9cfedd890c4c9ebd8897caa
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-12-20 21:04:00 -0500

    More!

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.options("*", cors(corsOptions)); // preflight OPTIONS; put before other rout
 
 const allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Header", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", true);
